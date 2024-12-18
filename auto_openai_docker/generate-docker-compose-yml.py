453cc9e025d1686802d6293c7a45b3a60eb652b3
jack.li <1050518702@qq.com>
2024-12-18 18:21:45 +0800

    update

--- auto_openai_docker/generate-docker-compose-yml.py
+++ auto_openai_docker/generate-docker-compose-yml.py
@@ -13,7 +13,8 @@ class Gen:
         "image": "harbor.uat.enflame.cc/library/enflame.cn/auto_openai:0.1",
         "environment": {
             "NODE_GPU_TOTAL": "{NODE_GPU_TOTAL}",
-            "GPU_TYPE": "{GPU_TYPE}"
+            "GPU_TYPE": "{GPU_TYPE}",
+            "GPU_DEVICE_ENV_NAME": "{GPU_DEVICE_ENV_NAME}"
         },
         "shm_size": "8gb",
         "command": [
@@ -33,15 +34,35 @@ class Gen:
 
     @classmethod
     def run(cls, gpu: list = [0], split_size=1, GPU_TYPE="EF-S60", image: str = ""):
+        if GPU_TYPE == "EF-S60":
+            GPU_DEVICE_ENV_NAME = "TOPS_VISIBLE_DEVICES"
+            deploy = {}
+        else:
+            GPU_DEVICE_ENV_NAME = "CUDA_VISIBLE_DEVICES"
+            deploy = {
+                "resources": {
+                    "reservations": {
+                        "devices": [
+                            {
+                                "driver": "nvidia",
+                                "count": "all",
+                                "capabilities": "[gpu]"
+                            }
+                        ]
+                    }
+                }
+            }
         containers = {}
         for idx, data in enumerate([gpu[i:i+split_size] for i in range(0, len(gpu), split_size)]):
             NODE_GPU_TOTAL = ",".join(map(str, data))
             container = dict(cls.default_container)
             environment = dict(container["environment"])
             environment.update(
-                {"NODE_GPU_TOTAL": NODE_GPU_TOTAL, "GPU_TYPE": GPU_TYPE})
+                {"NODE_GPU_TOTAL": NODE_GPU_TOTAL, "GPU_TYPE": GPU_TYPE, "GPU_DEVICE_ENV_NAME": GPU_DEVICE_ENV_NAME})
             container.update(
                 {"environment": environment, "shm_size": f"{8*len(data)}gb"})
+            if deploy:
+                container.update({"deploy": deploy})
             if image:
                 container.update({"image": image})
             containers[f"scheduler-{NODE_GPU_TOTAL.replace(',','_')}"] = container
@@ -55,11 +76,17 @@ class Gen:
 
 # size 是指一个实例挂载得卡数
 gcu_image = "harbor.uat.enflame.cc/library/enflame.cn/auto_openai:0.1"
-Gen.run(gpu=[0, 1, 2, 3, 4, 5, 6, 7], split_size=1)
-Gen.run(gpu=[0, 1, 2, 3, 4, 5, 6, 7], split_size=2)
-Gen.run(gpu=[0, 1, 2, 3, 4, 5, 6, 7], split_size=4)
-Gen.run(gpu=[0, 1, 2, 3, 4, 5, 6, 7], split_size=8)
-Gen.run(gpu=[0, 1, 2, 3], split_size=1)
-Gen.run(gpu=[0, 1, 2, 3], split_size=2)
-Gen.run(gpu=[0, 1, 2, 3], split_size=4)
-Gen.run(gpu=[4, 5], split_size=2)
+gpu_image = "registry.cn-shanghai.aliyuncs.com/zhph-server/auto_openai:0.1-cuda12.2"
+Gen.run(gpu=[0, 1, 2, 3, 4, 5, 6, 7], split_size=1,
+        GPU_TYPE="EF-S60", image=gcu_image)
+Gen.run(gpu=[0, 1, 2, 3, 4, 5, 6, 7], split_size=2,
+        GPU_TYPE="EF-S60", image=gcu_image)
+Gen.run(gpu=[0, 1, 2, 3, 4, 5, 6, 7], split_size=4,
+        GPU_TYPE="EF-S60", image=gcu_image)
+Gen.run(gpu=[0, 1, 2, 3, 4, 5, 6, 7], split_size=8,
+        GPU_TYPE="EF-S60", image=gcu_image)
+Gen.run(gpu=[0, 1, 2, 3], split_size=1, GPU_TYPE="EF-S60", image=gcu_image)
+Gen.run(gpu=[0, 1, 2, 3], split_size=2, GPU_TYPE="EF-S60", image=gcu_image)
+Gen.run(gpu=[0, 1, 2, 3], split_size=4, GPU_TYPE="EF-S60", image=gcu_image)
+Gen.run(gpu=[4, 5], split_size=2, GPU_TYPE="EF-S60", image=gcu_image)
+Gen.run(gpu=[0, 1, 2, 3], split_size=1, GPU_TYPE="NV-A100", image=gpu_image)
--- auto_openai_docker/stop.sh
+++ auto_openai_docker/stop.sh
@@ -4,6 +4,7 @@ docker-compose -f scheduler-0-1-2-3-4-5-6-7-of-2-GCU-docker-compose.yml down
 docker-compose -f scheduler-0-1-2-3-4-5-6-7-of-4-GCU-docker-compose.yml down
 docker-compose -f scheduler-0-1-2-3-4-5-6-7-of-8-GCU-docker-compose.yml down
 docker-compose -f scheduler-0-1-2-3-of-1-GCU-docker-compose.yml down
+docker-compose -f scheduler-0-1-2-3-of-1-GPU-docker-compose.yml down
 docker-compose -f scheduler-0-1-2-3-of-2-GCU-docker-compose.yml down
 docker-compose -f scheduler-0-1-2-3-of-4-GCU-docker-compose.yml down
 docker-compose -f scheduler-4-5-of-2-GCU-docker-compose.yml down
