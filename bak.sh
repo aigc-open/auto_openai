0c14b4b23b718a814afbc8015c170e939c0cdc49
jack.li <1050518702@qq.com>
2024-12-18 16:15:40 +0800

    update

--- bak.sh
+++ bak.sh
@@ -1,7 +1,17 @@
-rm -rf /workspace/code/github/auto_openai/*
-cp -rf ./* /workspace/code/github/auto_openai
-cp -f ./.gitignore /workspace/code/github/auto_openai
-cd /workspace/code/github/auto_openai
+project=/workspace/code/github/auto_openai
+rm -rf $project/*
+cp -rf ./* $project
+cp -f ./.gitignore $project
+cd $project
+git add .
+git commit -m "update"
+git push origin main
+
+project=/workspace/code/puhua/auto_openai
+rm -rf $project/*
+cp -rf ./* $project
+cp -f ./.gitignore $project
+cd $project
 git add .
 git commit -m "update"
 git push origin main
