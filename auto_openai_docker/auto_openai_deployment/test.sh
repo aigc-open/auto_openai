e64622d080c2169a14f6892deda859efdc5ec62d
jack.li <jack.li@enflame-tech.com>
2024-12-25 22:01:21 +0800

    update

--- auto_openai_docker/auto_openai_deployment/test.sh
+++ auto_openai_docker/auto_openai_deployment/test.sh
@@ -12,12 +12,12 @@ curl -X POST "$OPENAI_BASE_URL/chat/completions" \
   "stream": false
 }'
 
-curl -X POST "$OPENAI_BASE_URL/openai/v1/completions" \
+curl -X POST "$OPENAI_BASE_URL/completions" \
     -H "Authorization: Bearer xxxx" \
     -H "Content-Type: application/json" \
     -d '{
-  "model": "Qwen2.5-Coder-7B",
-  "prompt": "def print_hello",
+  "model": "deepseek-coder-6.7b-base",
+  "prompt": "# 打印冒泡排序 \ndef ",
   "max_tokens": 128,
   "temperature": 0.0,
   "stream": false
