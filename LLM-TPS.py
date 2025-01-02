5aa041fa411cceb8c7d196c0c0a70168ef79a1f4
jack.li <jack.li@enflame-tech.com>
2025-01-02 16:00:50 +0800

    update

--- LLM-TPS.py
+++ LLM-TPS.py
@@ -5,17 +5,17 @@ base_url = os.environ.get("OPENAI_BASE_URL", "http://127.0.0.1:9000/openai/v1")
 
 ########################### ###########################
 for model in [
-    "Qwen2.5-32B-Instruct-GPTQ-Int4",
-    "Qwen2.5-Coder-14B-Instruct",
-    "DeepSeek-Coder-V2-Lite-Instruct-8k",
-    "deepseek-coder-6.7b-instruct",
+    # "Qwen2.5-32B-Instruct-GPTQ-Int4",
+    # "Qwen2.5-Coder-14B-Instruct",
+    # "DeepSeek-Coder-V2-Lite-Instruct-8k",
+    # "deepseek-coder-6.7b-instruct",
     "Qwen2.5-7B-Instruct"
 ]:
 
     data = {"model": model,
             "messages": [
-                {"role": "user", "content": "What are some fun things to do in New York?"}],
-            "max_tokens": 1024,
+                {"role": "user", "content": "What are some fun things to do in New York?"*100000}],
+            "max_tokens": 8192,
             "temperature": 0.0,
             "stream": False}
     resp = requests.post(base_url + "/chat/completions", json=data)
