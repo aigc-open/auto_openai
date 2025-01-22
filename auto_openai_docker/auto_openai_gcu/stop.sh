e08cb3d323efb340b1c0586a5b1a26f64fde3ec0
jack.li <1050518702@qq.com>
2025-01-22 17:45:57 +0800

    update

--- auto_openai_docker/auto_openai_gcu/stop.sh
+++ auto_openai_docker/auto_openai_gcu/stop.sh
@@ -10,4 +10,14 @@ docker-compose -f scheduler-0-1-2-of-1-docker-compose.yml down
 docker-compose -f scheduler-3-3-3-3-of-1-docker-compose.yml down
 docker-compose -f scheduler-4-4-4-4-of-1-docker-compose.yml down
 docker-compose -f scheduler-4-5-of-2-docker-compose.yml down
-python3 stop.py
+
+for container in $(docker ps -a -q -f "label=auto_openai_all"); do    
+    # 停止容器（如果正在运行）
+    echo "正在停止容器... $container"
+    docker stop $container
+    
+    # 删除容器
+    echo "正在删除容器... $container"
+    docker rm $container
+    echo "===== 容器处理完成 ====="
+done
