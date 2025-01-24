fe1befa88881d9c2fd29d35103c765d22cb1381b
jack.li <1050518702@qq.com>
2025-01-24 12:04:07 +0800

    update

--- auto_openai_docker/auto_openai_deployment/stop.sh
+++ auto_openai_docker/auto_openai_deployment/stop.sh
@@ -12,11 +12,11 @@ docker-compose -f scheduler-3-3-3-3-of-1-docker-compose.yml down
 docker-compose -f scheduler-4-4-4-4-of-1-docker-compose.yml down
 for container in $(docker ps -a -q -f "label=auto_openai_all"); do    
     # 停止容器（如果正在运行）
-    echo "正在停止容器... $container"
+    echo "正在停止容器... "
     docker stop $container
     
     # 删除容器
-    echo "正在删除容器... $container"
+    echo "正在删除容器... "
     docker rm $container
     echo "===== 容器处理完成 ====="
 done
