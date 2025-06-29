f295d18f672f4c6b29a84be91277f9dfc367f560
jack.li <1050518702@qq.com>
2025-06-29 18:14:14 +0800

    Update Minio client configuration in create_bucket.sh to use port 9001 for improved service access.

--- auto_openai_docker/auto_openai_middleware/create_bucket.sh
+++ auto_openai_docker/auto_openai_middleware/create_bucket.sh
@@ -2,14 +2,14 @@ docker pull registry.cn-shanghai.aliyuncs.com/zhph-server/minio-mc:latest
 
 docker run --rm --network auto_openai_network \
   --entrypoint sh registry.cn-shanghai.aliyuncs.com/zhph-server/minio-mc:latest \
-  -c "mc alias set myminio http://minio:9000 admin admin123 && mc ls myminio"
+  -c "mc alias set myminio http://minio:9001 admin admin123 && mc ls myminio"
 
 
 docker run --rm --network auto_openai_network \
   --entrypoint sh registry.cn-shanghai.aliyuncs.com/zhph-server/minio-mc:latest \
-  -c "mc alias set myminio http://minio:9000 admin admin123 && mc mb myminio/api-platform"
+  -c "mc alias set myminio http://minio:9001 admin admin123 && mc mb myminio/api-platform"
 
 
 docker run --rm --network auto_openai_network \
   --entrypoint sh registry.cn-shanghai.aliyuncs.com/zhph-server/minio-mc:latest \
-  -c "mc alias set myminio http://minio:9000 admin admin123 && mc anonymous set public myminio/api-platform"
+  -c "mc alias set myminio http://minio:9001 admin admin123 && mc anonymous set public myminio/api-platform"
