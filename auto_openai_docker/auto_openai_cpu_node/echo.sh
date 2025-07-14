0160831a52b5497071408d7565872006634bcb6e
jack.li <jack.li@enflame-tech.com>
2025-07-14 20:14:17 +0800

    Remove docker-compose.yml file and update GPU_TYPE in config.yaml to CPU, reflecting changes in deployment configuration.

---
+++ auto_openai_docker/auto_openai_cpu_node/echo.sh
@@ -0,0 +1,28 @@
+# 获取本目录的scheduler*.yml文件，获取其名字
+scheduler_file=$(ls scheduler*.yml | head -n 100)
+
+
+# 循环启动
+echo "# down" > stop.sh
+for file in $scheduler_file
+do
+    echo "docker-compose -f $file down" >> stop.sh
+done
+echo "# up" > start.sh
+for file in $scheduler_file
+do
+    echo "# docker-compose -f $file up -d" >> start.sh
+done
+cat << 'EOF' >> stop.sh
+for container in $(docker ps -a -q -f "label=auto_openai_all"); do    
+    # 停止容器（如果正在运行）
+    echo "正在停止容器... "
+    docker stop $container
+    
+    # 删除容器
+    echo "正在删除容器... "
+    docker rm $container
+    echo "===== 容器处理完成 ====="
+done
+EOF
+echo "Done"
---
+++ auto_openai_docker/auto_openai_cpu_node/pull.sh
@@ -0,0 +1,2 @@
+docker pull registry.cn-shanghai.aliyuncs.com/zhph-server/auto_openai:shdx
+bash pull-gpu-image.sh
