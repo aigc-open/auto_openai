df8eafe7ecb456a6df9b0166808cfd87d9747f85
jack.li <1050518702@qq.com>
2025-07-14 20:40:57 +0800

    Uncomment docker-compose command in start.sh to enable service startup for scheduler configuration.

--- auto_openai_docker/auto_openai_cpu_node/start.sh
+++ auto_openai_docker/auto_openai_cpu_node/start.sh
@@ -1,5 +1,5 @@
 # up
-# docker-compose -f scheduler-0-1-2-3-of-1-docker-compose.yml up -d
+docker-compose -f scheduler-0-1-2-3-of-1-docker-compose.yml up -d
 # docker-compose -f scheduler-0-1-2-3-of-2-docker-compose.yml up -d
 # docker-compose -f scheduler-0-1-2-3-of-4-docker-compose.yml up -d
 # docker-compose -f scheduler-0-1-of-1-docker-compose.yml up -d
